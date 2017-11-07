c7bc037b1675eeb3653e2badb430b52d077ed252
Guillaume Vareille <tinyfiledialogs@ysengrin.com>
2017-11-07 16:19:13 +0100

    v1.0.2

--- toot.c
+++ toot.c
@@ -1,5 +1,5 @@
 /* __              __ 
-  / /_____  ____  / /_  toot.c v1.0.1 [Nov 7, 2017] zlib licence
+  / /_____  ____  / /_  toot.c v1.0.2 [Nov 7, 2017] zlib licence
  / __/ __ \/ __ \/ __/  cross-platform library and command line tool to toot "tooooot"
 / /_/ /_/ / /_/ / /_    file created [November 7, 2017]
 \__/\____/\____/\__/    Copyright (c) 2017 Guillaume Vareille http://ysengrin.com
@@ -34,7 +34,7 @@ misrepresented as being the original software.
 #endif
 #include <stdio.h>
 
-char toot_version[8] = "1.0.1"; /* contains tinyfd current version number */
+char toot_version[8] = "1.0.2"; /* contains tinyfd current version number */
 
 int toot_verbose = 0; /* 0 (default) or 1 : prints the command line calls */
 
